06ed2780d2c8b4485393e30ea53d70e671ce8dff
Julio Cesar <rasjuliocesar@gmail.com>
2021-05-30 11:35:47 -0300

    Ajuste CSS e HTML, Add método Count*()

--- src/app/dashboard/ler-dashboard/ler-dashboard.component.ts
+++ src/app/dashboard/ler-dashboard/ler-dashboard.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
+//import { Song } from '../../models/song.model'
+//import { SongService } from '../../services/song.service';
+
+import { Song } from 'src/app/models/song.model';
+import { SongService } from 'src/app/services/song.service';
+import { Artists } from '../../models/artists.model'
+import { ArtistsService } from '../../services/artists.service';
+import { Genre } from '../../models/genre.model'
+import { GenreService } from '../../services/genre.service';
+
 @Component({
   selector: 'app-ler-dashboard',
   templateUrl: './ler-dashboard.component.html',
@@ -7,9 +18,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LerDashboardComponent implements OnInit {
 
-  constructor() { }
+  artist: Artists[]
+  genre: Genre[]
+  song: Song[]
+
+  constructor(private artistsService: ArtistsService, private songService: SongService, private genreService: GenreService) { }
 
   ngOnInit(): void {
-  }
+    this.artistsService.contarArtistas().subscribe(artist => {
+      this.artist = artist})
+      
+    this.genreService.contarGeneros().subscribe(genre => {
+      this.genre = genre})
 
+    this.songService.contarMusicas().subscribe(song => {
+      this.song = song})
+  }
 }
